99b96c06eb4afb1783f7fe746742d1fdbbbf077c
Nguyen Sang <nnsang24@gmail.com>
2018-12-17 09:47:07 +0700

    change

--- app/controllers/check.js
+++ app/controllers/check.js
@@ -4,21 +4,37 @@ const md5 = require('md5');
 var fs = require('fs');
 
 router.post('/' , (req, res, next) => {
-
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE');
+	res.header('Access-Control-Allow-Headers', 'Content-Type');
+	
+	console.log(res.body);
+	
 	var obj;
 	fs.readFile('./db/users.json', 'utf8' , function(err , data){
 		if (err) throw err;
 		obj = JSON.parse(data)
+
+		console.log(req.body);
+		
 		let username = req.body.usersname;
-		let password = md5(req.body.password);
-		let flag = 0
+		// let password = md5(req.body.password);
+		let flag = 0;
+		let datatmp = {};
 		for (let key in obj) {
 			if (obj.hasOwnProperty(key)) {
 				let objUsers = obj[key].username
-				let objPassword = obj[key].password
+				// let objPassword = obj[key].password
+				let objDes = obj[key].des
 				if (objUsers === username) {
 					if (objPassword === password) {
 						flag = 1;
+						datatmp = {
+							username : objUsers,
+							// password: objPassword,
+							description: objDes,
+							messages: 'Login success'
+						}
 						break;
 					} else {
 						flag = 2;
@@ -29,7 +45,7 @@ router.post('/' , (req, res, next) => {
 			}
 		}
 
-		flag === 1 ? res.redirect('/') : res.redirect('/login')
+		flag === 1 ? res.json({status: 200 , data: datatmp}) : res.json({status: 404 , data: datatmp})
 		
 	})
 })
--- scripts/index.js
+++ scripts/index.js
@@ -48,36 +48,62 @@ let indexApp = {
 				<div class="block__des">Lorem</div>
 			</div>
 			<div class="block__action">
-				<div class="block__file"><a href="#">Xem file</a></div>
-				<div class="block__btn--collapse"><button type="button"><i class="mdi mdi-chevron-down"></i></button></div>
-				<div class="block__btn--moveTask"><button type="button"><i class="mdi mdi-chevron-right"></i></button></div>
+				<div class="block__file">
+					<a href="#">Xem file</a>
+				</div>
+				<div class="block__btn--collapse">
+					<button type="button">
+						<i class="mdi mdi-chevron-down"></i>
+					</button>
+				</div>
+				<div class="block__btn--moveTask">
+					<button type="button">
+						<i class="mdi mdi-chevron-right"></i>
+					</button>
+				</div>
 			</div>
 		</div>
 		<div class="block__collapse">
 			<div class="block__detail">
 				<hr>
 				<div class="row">
-					<div class="col"><i class="mdi mdi-briefcase"></i>Chua xac dinh</div>
-					<div class="col-auto"><span>3h</span></div>
-					<div class="col-auto"><span>Chua xac dinh</span></div>
+					<div class="col">
+						<i class="mdi mdi-briefcase"></i>
+						Chua xac dinh
+					</div>
+					<div class="col-auto">
+						<span>3h</span>
+					</div>
+					<div class="col-auto">
+						<span>Chua xac dinh</span>
+					</div>
 				</div>
 				<hr>
 			</div>
 			<div class="block__create">
 				<div class="row">
-					<div class="col"><i class="mdi mdi-account"></i><a href="#">Ngoc Sang</a></div>
-					<div class="col-auto"><span class="creat__day">6 months ago</span></div>
+					<div class="col">
+						<i class="mdi mdi-account"></i>
+						<a href="#">Ngoc Sang</a>
+					</div>
+					<div class="col-auto">
+						<span class="creat__day">6 months ago</span>
+					</div>
 				</div>
 				<hr>
 				<div class="row">
-					<div class="col"><i class="mdi mdi-pen"></i><a href="#">Anh dao</a></div>
-					<div class="col-auto"><span class="creat__day">6 months ago</span></div>
+					<div class="col">
+						<i class="mdi mdi-pen"></i>
+						<a href="#">Anh dao</a>
+					</div>
+					<div class="col-auto">
+						<span class="creat__day">6 months ago</span>
+					</div>
 				</div>
 			</div>
 		</div>
 	</div>`
-	}
-	,
+	},
 	creatLists: () => {
 		let tmp = []
 		for (let key in data) {
@@ -88,13 +114,13 @@ let indexApp = {
 		}
 		return tmp;
 	},
-	shortTable: (id, opt) => {
+	enableShortTable: (id, opt) => {
 		let el = document.getElementById(id)
 		let sortable = new Sortable(el, opt)
 	},
 	initShortTable: () => {
 		for (let key in indexApp.listIdShortTable) {
-			indexApp.shortTable(key, indexApp.listIdShortTable[key])
+			indexApp.enableShortTable(key, indexApp.listIdShortTable[key])
 			let el = document.getElementById(key)
 			el.innerHTML += indexApp.creatLists()
 			// console.log(el);
